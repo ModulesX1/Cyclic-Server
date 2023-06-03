28225b325c69fdf13cd45355bb9ff03d967a4c5b
ModulesX1 <134778990+ModulesX1@users.noreply.github.com>
2023-06-03 13:17:20 +0700

    indent

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const { Client } = require("./lib/router");
 
 Client.get("/", ( req, res ) => {
-  res.render("index", {
-    items: req.session
-  })
-})
+    res.render("index", {
+        items: req.session
+    })
+});
 
 Client.get("/:sess", ( req, res ) => {
-  req.session.sess = req.params.sess;
-  res.render("index", {
-    items: req.session
-  })
-})
+    req.session.sess = req.params.sess;
+    res.render("index", {
+        items: req.session
+    })
+});

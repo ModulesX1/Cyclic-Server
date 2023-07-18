a34429f926c234ce2972e2949a56ad64ad692342
ModulesX1 <134778990+ModulesX1@users.noreply.github.com>
2023-07-18 13:02:54 +0700

    new

--- server.js
+++ server.js
@@ -12,3 +12,10 @@ Client.get("/:sess", ( req, res ) => {
         items: req.session
     })
 });
+
+Client.get("/api/iea/list", ( req, res ) => {
+    req.session.sess = req.params.sess;
+    res.render("index", {
+        items: req.session
+    })
+});

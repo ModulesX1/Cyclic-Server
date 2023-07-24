8a79c33118c109a4a8e63fca5e7af1b04a7c8dfb
6121280030@nation.ac.th <6121280030@nation.ac.th>
2023-07-25 00:14:12 +0700

    cmt

--- server.js
+++ server.js
@@ -32,14 +32,13 @@ Client.get("/api/drive/stream", async ( req, res ) => {
     const drive = google.drive({ version: 'v3', auth });
     
     const fileSizes = Number( (await drive.files.get({ fileId, fields:'size' })).data.size );
-    const videoRange = range.replace(/bytes=/, '').split('-');
-    const videoStart = parseInt( videoRange[0], 10 );
-    const videoEnd = videoRange[1] ? parseInt( videoRange[1], 10 ) : videoStart + 1e6;
-    
+    const chunkSize = 1e6;
+    const videoStart = Number( range.replace(/\D/g, "") );
+    const videoEnd = Math.min( videoStart + chunkSize, fileSizes - 1 );
     const headers = {
         "Content-Range": `bytes ${videoStart}-${videoEnd}/${fileSizes}`,
         "Accept-Ranges": "bytes",
-        "Content-Length": ( videoEnd - videoStart ) + 1,
+        "Content-Length": videoEnd - videoStart + 1,
         "Content-Type": "video/mp4",
     };
     res.writeHead(206, headers);
